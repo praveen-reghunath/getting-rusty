216afb93eddce38e6b5bf287026cd2ba829c3eda
praveen-reghunath <6327769+praveen-reghunath@users.noreply.github.com>
2026-02-15 12:48:04 -0800

    Fixed warnings

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,13 +1,13 @@
 fn main() {
     println!("Hello, world!");
+    print!("Hello world, again!");
+    let mut _str = String::from("Hello, Rust!");
 
-    let mut str = String::from("Hello, Rust!");
+    let mut _str = 4;
 
-    let mut str = 4;
+    let _str = "Hello, world!";
 
-    let str = "Hello, world!";
-
-    println!("{}", str);
+    println!("{}", _str);
 
     let age = 30;
     println!("I am {} years old.", age);
